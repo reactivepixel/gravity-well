ab6f4ea9c5ac49ed99083b3d210f6579973c43d5
Chris Chapman <Chapman@apextion.com>
2017-11-21 16:11:40 -0500

    parsing hook slightly

--- src/routes/hooks/index.js
+++ src/routes/hooks/index.js
@@ -4,6 +4,19 @@ module.exports = (express) => {
 
   router.post('/max/pr', (req, res) => {
     util.log('Incomming Hook', req.body);
+//    req.body.hook.
+    const githubBaseURL = 'https://api.github.com/repos/reactivepixel/gravity-well/'
+    const branches = require(githubBaseURL + 'branches');
+    let commitURL;
+    branches.map((branch) => {
+      if(branch.name === 'release'){
+        commitURL = branch.commit.url;
+      }
+      return branch;
+    })
+
+    util.log('Matched Release Branch Commit URL', commitURL)
+
     res.json({
       healthy: true,
     });
