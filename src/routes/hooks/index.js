03b38dbca3542d9930ce04067a1fc5254b23c1f3
Chris Chapman <Chapman@apextion.com>
2017-11-21 15:57:58 -0500

    added pr hook

--- src/routes/hooks/index.js
+++ src/routes/hooks/index.js
@@ -2,7 +2,7 @@ module.exports = (express) => {
   const router = express.Router();
   const util = require('apex-util');
 
-  router.post('/max', (req, res) => {
+  router.post('/max/pr', (req, res) => {
     util.log('Incomming Hook', req.body);
     res.json({
       healthy: true,
